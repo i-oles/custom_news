147a0000a7f475ba8a2d2e48b1dadab7825773cb
Igor Oles <igor.oles@gmail.com>
2020-07-29 13:51:28 +0200

    links_and_subjects_dict added

--- oko_press.py
+++ oko_press.py
@@ -4,55 +4,56 @@ import requests
 source = requests.get('https://oko.press').text
 soup = BeautifulSoup(source, 'lxml')
 
-all_articles_links = []
+# correct it \n thing
+articles_links_and_subjects_dict = {}
 
 main_article_link = soup.find('div', class_='home-page').a.attrs['href']
-all_articles_links.append(main_article_link)
+main_article_subject = soup.find('span', class_='subcategory-title').text
+articles_links_and_subjects_dict[main_article_link] = main_article_subject
 
 for div_tag in soup.find_all('div', class_='large-collapse'):
     try:
         next_article_link = div_tag.find('h4').a.attrs['href']
+        next_article_subject = div_tag.find('div', class_='sub-category-name').text
     except:
         pass
-    all_articles_links.append(next_article_link)
+    articles_links_and_subjects_dict[next_article_link] = next_article_subject
 
-NUM_OF_SELECTED_ARTICLES = 6
+#NUM_OF_SELECTED_ARTICLES = 6
+#for article_link in all_articles_links[:NUM_OF_SELECTED_ARTICLES]:
+for article_link in all_articles_links:
 
-for article_link in all_articles_links[:NUM_OF_SELECTED_ARTICLES]:
-    try:
-        article_subject = soup.find('div', class_='sub-category-name').text
-        print(f"Subject of article: {article_subject}")
-        print('\n')
-    except:
-        article_subject = soup.find('span', class_='subcategory-title').text
-        print(f"Subject of article: {article_subject}")
-        print('\n')
+# correct it: print(f"Subject of article: {article_subject}")
 
     article_site = requests.get(article_link).text
     article_site = BeautifulSoup(article_site, 'lxml')
 
-    article_date = article_site.find(‘time’, class_=’updated’).text
-    print(f’article date: {article_date}’)
+# correct it: different tag then time
+
+    article_date = article_site.find('time', class_='updated').text
+    print(f'article date: {article_date}')
 
     article_author = article_site.find('span', class_='meta-section__autor').a.text
-    print(f’article author: {article_author}’)
+    print(f'article author: {article_author}')
 
-    article_headline = article_site.find(‘h1’, class_=’title smaller-post-title’).span.text
+    article_headline = article_site.find('h1', class_='title smaller-post-title').span.text
     print(article_headline)
 
     try:
         image_link = article_site.find('div', class_='slider_home_page slider_in_post')
         image_link = image_link.picture.source['data-srcset']
         print(image_link)
+        # add image author
     except AttributeError:
         print("There is no image added to this article.")
-
+    
     try:
         video_link = article_site.find('div', class_='off-canvas-wrapper')
         video_link = video_link.find('iframe')['src']
         print("Link to video:")
         print(video_link)
-    except AttributeError, TypeError, KeyError:
+    # AttributeError, TypeError, KeyError
+    except:
         video_link = None
     print('\n')
 
@@ -82,3 +83,4 @@ for article_link in all_articles_links[:NUM_OF_SELECTED_ARTICLES]:
 
     all_paragraphs = ('\n').join(all_paragraphs_list)
     print(all_paragraphs)
+
---
+++ test.py
@@ -0,0 +1,9 @@
+my_dict = {
+    'link_1' : 'subject_1',
+    'link_2' : 'subject_2',
+    'link_3' : 'subject_3',
+}
+
+for link, subject in my_dict.items():
+    print(link)
+    print(subject)
